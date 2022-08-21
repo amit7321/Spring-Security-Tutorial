4766fb8d5e0f9b43b3fcd1fb36416ee9955babed
amit <amit.hasan7321@gmail.com>
2022-08-21 12:04:37 +0600

    Student class is iniatialized. Variables are initialized

---
+++ src/main/java/com/example/Spring_Security_Tutoiral/SpringSecurityTutoiralApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Security_Tutoiral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTutoiralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTutoiralApplication.class, args);
+	}
+
+}
