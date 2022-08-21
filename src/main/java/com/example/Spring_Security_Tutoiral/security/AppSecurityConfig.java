fa770a50dd287607ce2c489abffdb8e5729861d1
amit <amit.hasan7321@gmail.com>
2022-08-21 16:36:48 +0600

    Spring security initialized in the project. Basic authentication is used. User have to login by providing username and password.

--- src/main/java/com/example/Spring_Security_Tutoiral/security/AppSecurityConfig.java
+++ src/main/java/com/example/Spring_Security_Tutoiral/security/AppSecurityConfig.java
@@ -1,4 +1,26 @@
 package com.example.Spring_Security_Tutoiral.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class AppSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+
 }
