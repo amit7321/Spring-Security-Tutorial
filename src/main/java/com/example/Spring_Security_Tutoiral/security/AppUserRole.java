efe58b0ced59181fbcf6e995c0ed44c5b2f23e32
amit <amit.hasan7321@gmail.com>
2022-10-03 19:39:58 +0600

    implement hasAuthority to security config.

--- src/main/java/com/example/Spring_Security_Tutoiral/security/AppUserRole.java
+++ src/main/java/com/example/Spring_Security_Tutoiral/security/AppUserRole.java
@@ -1,14 +1,16 @@
 package com.example.Spring_Security_Tutoiral.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.example.Spring_Security_Tutoiral.security.AppUserPermission.*;
 
 public enum AppUserRole {
     student(Sets.newHashSet()),
-    admin(Sets.newHashSet(course_write, course_read, student_read, student_write)),
+    admin(Sets.newHashSet(course_write, course_read, student_read, student_write )),
     adminTrainee(Sets.newHashSet(course_read, student_read));
 
     private final Set<AppUserPermission> permissions;
@@ -19,5 +21,15 @@ public enum AppUserRole {
 
     public Set<AppUserPermission> getPermissions() {
         return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities()
+    {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE" + this.name()));
+
+         return permissions;
     }
 }
